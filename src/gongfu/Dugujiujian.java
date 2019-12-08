12aa8bf4776bd46ed329ee2b327892b82d1a4ba5
yuebuqun <yuebuqun3333@hainan.net>
2019-12-08 21:51:47 +0800

    岳不群再次修改文件

--- src/gongfu/Dugujiujian.java
+++ src/gongfu/Dugujiujian.java
@@ -5,6 +5,7 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("1111111111");
 		System.out.println("2222222222");
+		System.out.println("4444444444");
 	}
 
 }
